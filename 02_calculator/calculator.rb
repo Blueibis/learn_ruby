47bbc30c2cb1c35ecf2234d20c58b438c4704894
Blueibis <jennie.li.2011@gmail.com>
2017-05-27 14:18:32 -0400

    02 extras passed

--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,10 +1,10 @@
 def add(x, y)
   x + y
-end
+end            # => :add
 
 def subtract(x, y)
   x - y
-end
+end                 # => :subtract
 
 def sum(numbers)
   total = 0
@@ -12,4 +12,36 @@ def sum(numbers)
     total += number
   end
   total
+end                         # => :sum
+
+def multiply(numbers)
+  total = nil
+  numbers.each do |number|
+    if total == nil
+      total = number
+    else
+      total *= number
+  end
 end
+  total
+end                         # => :multiply
+
+def factorial(number)
+  if number == 0 || number == 1  # => false
+    arr = [1, 1]
+  else
+  arr = (1..number).to_a         # => [1, 2, 3, 4, 5]
+  arr.reverse!                   # => [5, 4, 3, 2, 1]
+  end                            # => [5, 4, 3, 2, 1]
+  factor = nil                   # => nil
+  arr.each do |n|                # => [5, 4, 3, 2, 1]
+    if factor == nil             # => true, false, false, false, false
+      factor = n                 # => 5
+    else
+      factor *= n                # => 20, 60, 120, 120
+    end                          # => 5, 20, 60, 120, 120
+  end                            # => [5, 4, 3, 2, 1]
+    return factor                # => 120
+end                              # => :factorial
+
+factorial(5)  # => 120
